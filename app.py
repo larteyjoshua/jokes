62fc3f5900da4953e441abaa05f1f5142d0739db
Lartey Joshua <larteyjoshua@gmail.com>
2019-07-29 20:34:41 +0000

    Simple Fask Web App Using Joke  API

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import *
+import requests,json
+
+app = Flask(__name__)
+jokers=[]
+@app.route('/')
+def index():
+    return render_template("jokes.html", jokers=jokers)
+
+@app.route('/jokes', methods=["POST"])
+def jokes():
+    if request.method == "POST":
+        firstname= request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        print(firstname)
+        print(lastname)
+        r=requests.get('http://api.icndb.com/jokes/random?firstName='+firstname + '&lastName='+lastname)
+        print(r.text)
+        data=json.loads(r.text)
+        yourjoke= data.get('value').get('joke')
+        print(yourjoke)
+        jokers.append(yourjoke)
+        return redirect("/", code=302)
+
+
+if __name__ == '__main__':
+    app.run()
